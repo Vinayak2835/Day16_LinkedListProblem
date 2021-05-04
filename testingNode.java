ab89018062b15be647474c2bef42a1e8e471f25d
Vinayak <80584437+Vinayak2835@users.noreply.github.com>
2021-05-04 20:26:57 +0530

    [Vinayak] Added: Test Cases for Linked List added

---
+++ testingNode.java
@@ -0,0 +1,47 @@
+package com.linkedlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.LinkedList;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class testingNode {
+
+	 @Test
+	    public void returnTrueWhenFindNode() {
+	        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+	        Integer a = 56, b = 30, c = 70;
+	        linkedList.append(a);
+	        linkedList.append(c);
+	        linkedList.insertInBetween(b, a);
+	        Assert.assertEquals(true, linkedList.findNode(b));
+	    }
+	    /**
+	     * ability to insert element after given value node key
+	     */
+	    @Test
+	    public void insertNumberAfterGivenValue() {
+	        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+	        Integer a = 56, b = 30, c = 70 ;
+	        linkedList.append(a);
+	        linkedList.append(c);
+	        linkedList.insertNumberAfterGivenNodeKey(b, a);
+	        Assert.assertEquals(true, linkedList.findNode(b));
+	    }
+
+	    /**
+	     * ability to delete element from LL sequence
+	     */
+	    @Test
+	    public void deleteGivenNode() {
+	        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+	        Integer a = 56, b = 30, c = 70;
+	        linkedList.append(a);
+	        linkedList.append(c);
+	        linkedList.insertNumberAfterGivenNodeKey(b, a);
+	        linkedList.deleteNode(b);
+	        Assert.assertEquals(false, linkedList.findNode(b));
+	    }
+}
